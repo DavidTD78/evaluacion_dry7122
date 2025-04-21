78a956a25dda9d90cb2f63bd57d061039835d853
DavidTD78 <da.juarez@duocuc.cl>
2025-04-21 00:48:38 +0000

    Se crearon los .py de presentación, verificación de puerto, datos_usuario.

---
+++ presentacion.py
@@ -0,0 +1,7 @@
+participantes_prueba = ["David Juarez", "Ismael Consuegra"]
+
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("Participantes:")
+
+for nombre in participantes_prueba:
+    print(nombre)
